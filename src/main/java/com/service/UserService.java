b8aeef4a3d88091ec87b261c20a95a3c03248cb0
DDU <DDU@CIO-EB84305SW.sba.gov>
2019-11-17 21:25:40 -0500

    added log4j

--- src/main/java/com/service/UserService.java
+++ src/main/java/com/service/UserService.java
@@ -1,13 +1,16 @@
 package com.service;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+	private Logger logger = Logger.getLogger(UserService.class);	
 	public boolean IsValidUser(String UserName, String Password) {
 		if (UserName.equals("springuser1@spring.com")
 				&& Password.equals("springpwd1"))
 			return true;
+		logger.info("User Name: "+UserName + " and Password: " + Password+" combination is incorrect.");
 		return false;
 	}
 }
